cd4512ab81cdeceb33b4dca07b9d097c89fe1117
Stefan Glargaard Darling <stefarling@gmail.com>
2023-11-24 19:40:24 +0100

    Created packaging tool step 1

---
+++ package.vbs
@@ -0,0 +1,2 @@
+Set objShell = WScript.CreateObject("WScript.Shell")
+objShell.Run "cmd /c packager.bat", 0, True
---
+++ packager.bat
@@ -0,0 +1,19 @@
+@echo off
+del .\compiled\assembled\* /q /s
+xcopy  .\compiled\*.exe .\compiled\assembled\*
+xcopy  .\LICENSE* .\compiled\assembled\LICENSE*
+xcopy  .\README* .\compiled\assembled\README*
+xcopy  .\CHANGELOG* .\compiled\assembled\CHANGELOG*
+xcopy  .\assets\Scripts .\compiled\assembled\Scripts /S /E /Q /Y /I /R /K
+
+set "folderPath=.\compiled\assembled"
+
+REM Get the name of the first file in the "assembled" folder that matches the pattern "AHKser*"
+for %%F in ("%folderPath%\AHKser*.exe") do set "baseFileName=%%~nF"
+
+cd "%folderPath%"
+
+REM Use PowerShell to create a zip file
+powershell Compress-Archive -Path * -DestinationPath "..\..\%baseFileName%.zip"
+
+echo Zip file %baseFileName%.zip has been created in %folderPath%
